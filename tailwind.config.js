ba99a01495074c4132c45e93031cc1028080f749
shivayogeppa <shivuchanagond25@gmail.com>
2026-08-20 01:32:36 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#6366f1",
+      },
+    },
+  },
+  plugins: [],
+}
